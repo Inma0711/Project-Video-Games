563cdadb2d048e6a79ffe3fb2926083f1e7d0b3d
Inma Gonzalez <inmagonzalez045@gmail.com>
2024-10-29 14:19:53 +0100

    test

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const	REQUEST = "../json/videoGames.json";
+
+async function	fetchVideoGamesJson() 
+{
+	const	response = await fetch(REQUEST);
+
+	try
+	{
+		if (!response.ok) {
+			throw new Error("Error: file not found: ", response.status);
+		}
+		return await response.json();
+	}
+	catch (error)
+	{
+		console.error(error);
+		return null;
+	}
+}
+
+function	createVideoGameCards({image, description, price})
+{
+	return `
+		<div class="card" style="width: 18rem;">
+			<img src="${image}" class="card-img-top" alt="A videogame image">
+			<div class="card-body">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text">${description}</p>
+				<a href="#" class="btn btn-primary">${price}</a>
+			</div>
+		</div>
+	`;
+}
+
+async function	displayVideoGames()
+{
+	const	games = document.getElementById("games");
+	const	videoGamesData = await fetchVideoGamesJson();
+	let		videoGameCards;
+
+	if (!videoGamesData || !videoGamesData.videoGames)
+	{
+		games.innerHTML = "<p>Error: couldn't load image correctly</p>";
+		return ;
+	}
+	videoGameCards = videoGamesData.videoGames.map(createVideoGameCards).join('');
+	games.innerHTML = videoGameCards;
+}
+
+displayVideoGames();
